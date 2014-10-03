7171f76b34f47644639b9f9c87edcd373e2d9741
Paul D'Albora <pdalbora@guidewire.com>
2014-10-02 21:22:48 -0700

    Allow unqualified access to typekeys
    Typekeys are no longer enums, but we still want them to have the
    benefits of unqualified access. Change the parser so that it allows this
    both for enum types and where the value type is an IEnumValue.

--- gosu-core/src/main/java/gw/internal/gosu/parser/GosuParser.java
+++ gosu-core/src/main/java/gw/internal/gosu/parser/GosuParser.java
@@ -141,6 +141,7 @@ import gw.lang.reflect.ICanBeAnnotation;
 import gw.lang.reflect.IConstructorInfo;
 import gw.lang.reflect.IConstructorType;
 import gw.lang.reflect.IEnumType;
+import gw.lang.reflect.IEnumValue;
 import gw.lang.reflect.IErrorType;
 import gw.lang.reflect.IFunctionType;
 import gw.lang.reflect.IInvocableType;
@@ -5621,13 +5622,11 @@ public final class GosuParser extends ParserBase implements IGosuParser
     IType contextType = getContextType().getType();
     if( contextType != null )
     {
-      if( contextType.isEnum() )
+      if( contextType.isEnum() || TypeSystem.get(IEnumValue.class).isAssignableFrom(contextType) )
       {
-        IEnumType enumAccess = (IEnumType)contextType;
-
         try
         {
-          IPropertyInfo property = enumAccess.getTypeInfo().getProperty( strConstValue );
+          IPropertyInfo property = contextType.getTypeInfo().getProperty( strConstValue );
           if( property != null && property.isStatic() )
           {
             MemberAccess ma = new MemberAccess();
