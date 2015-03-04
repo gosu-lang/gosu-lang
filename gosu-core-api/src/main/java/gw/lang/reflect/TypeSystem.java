2e8b8bd949e960a8045ea0ada5276115e8fddd30
Luca Boasso <lboasso@guidewire.com>
2015-03-04 14:00:26 -0800

    Fix deadlock

--- gosu-core-api/src/main/java/gw/lang/reflect/TypeSystem.java
+++ gosu-core-api/src/main/java/gw/lang/reflect/TypeSystem.java
@@ -44,7 +44,7 @@ import java.util.concurrent.locks.ReentrantLock;
 @UnstableAPI
 public class TypeSystem
 {
-  private static final Lock GLOBAL_LOCK = new ReentrantLock();
+  private static final Lock GLOBAL_LOCK = new TypeSystemLock();
   public static InvocationCounter tyeRequestCounter = new InvocationCounter(false);
   public static InvocationCounter tyeLoadingCounter = new InvocationCounter(false);
 
