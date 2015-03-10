ee39b47b6f893e94a25c5a8af3955e0be08551e8
smckinney <rsmckinney@hotmail.com>
2015-03-10 13:44:28 -0700

    Revert "use a reentrant lock for the  interim while the type system may be initializing"
    This reverts commit 34738dab973e03157e9b62b0eb03cdecef80aa6f.

--- gosu-core-api/src/main/java/gw/lang/reflect/TypeSystemLock.java
+++ gosu-core-api/src/main/java/gw/lang/reflect/TypeSystemLock.java
@@ -1,46 +1,19 @@
 package gw.lang.reflect;
 
-import gw.lang.init.GosuInitialization;
 import gw.util.GosuUnsafeUtil;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  */
 public class TypeSystemLock implements Lock
 {
-  private Lock _interimLock = new ReentrantLock();
-  private ThreadLocal<Integer> _interimCount = new ThreadLocal<>();
-  private volatile boolean _initialized = false;
-
   @Override
   public void lock()
   {
-    if( initializing() )
-    {
-      _interimLock.lock();
-      Integer count = _interimCount.get();
-      if( count == null )
-      {
-        count = 0;
-      }
-      _interimCount.set( ++count );
-    }
-    else
-    {
-      GosuUnsafeUtil.monitorEnter( getMonitor() );
-      _initialized = true;
-    }
-  }
-
-  private boolean initializing()
-  {
-    return !_initialized &&
-           (!GosuInitialization.isAnythingInitialized() ||
-            !GosuInitialization.instance( TypeSystem.getExecutionEnvironment() ).isInitialized());
+    GosuUnsafeUtil.monitorEnter( getMonitor() );
   }
 
   @Override
@@ -52,28 +25,19 @@ public class TypeSystemLock implements Lock
   @Override
   public boolean tryLock()
   {
-    return GosuUnsafeUtil.tryMonitorEnter( getMonitor() );
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public boolean tryLock( long time, TimeUnit unit ) throws InterruptedException
   {
-    return tryLock();
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void unlock()
   {
-    Integer count = _interimCount.get();
-    if( count != null && count != 0 )
-    {
-      _interimCount.set( --count );
-      _interimLock.unlock();
-    }
-    else
-    {
-      GosuUnsafeUtil.monitorExit( getMonitor() );
-    }
+    GosuUnsafeUtil.monitorExit( getMonitor() );
   }
 
   @Override
