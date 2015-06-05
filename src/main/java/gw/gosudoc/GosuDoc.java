2efc261fd211eba9d56f9a5f9bb5fb0c75cc821b
Carson Gross <carsongross@gmail.com>
2015-06-05 12:22:36 -0700

    Update initialization code

--- src/main/java/gw/gosudoc/GosuDoc.java
+++ src/main/java/gw/gosudoc/GosuDoc.java
@@ -5,13 +5,15 @@ import gw.lang.cli.CommandLineAccess;
 import gw.lang.reflect.ReflectUtil;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class GosuDoc
 {
   public static void main( String[] args ) throws IOException
   {
-    Gosu.init();
+    Gosu.bootstrapGosuWhenInitiatedViaClassfile();
     Object gosuDocArgs = ReflectUtil.construct( "gw.gosudoc.cli.GosuDocArgs" );
+    CommandLineAccess.setRawArgs( Arrays.asList(args) );
     CommandLineAccess.initialize( gosuDocArgs, true );
     ReflectUtil.construct( "gw.gosudoc.GSDocHTMLWriter", gosuDocArgs );
   }
