20c9d88a5a9186c5a526907fff1b22305acd6374
smckinney <rsmckinney@hotmail.com>
2015-06-17 13:17:24 -0700

    fix compiler bug for elvis-expr, don't double-evaluate condition

--- gosu-core/src/main/java/gw/internal/gosu/ir/transform/expression/ConditionalTernaryExpressionTransformer.java
+++ gosu-core/src/main/java/gw/internal/gosu/ir/transform/expression/ConditionalTernaryExpressionTransformer.java
@@ -8,6 +8,8 @@ import gw.internal.gosu.parser.expressions.ConditionalTernaryExpression;
 import gw.internal.gosu.ir.transform.ExpressionTransformer;
 import gw.internal.gosu.ir.transform.TopLevelTransformationContext;
 import gw.lang.ir.IRExpression;
+import gw.lang.ir.IRSymbol;
+import gw.lang.ir.statement.IRAssignmentStatement;
 
 /**
  */
@@ -26,22 +28,27 @@ public class ConditionalTernaryExpressionTransformer extends AbstractExpressionT
 
   protected IRExpression compile_impl()
   {
-    IRExpression condition;
     if( _expr().getCondition() == null )
     {
       // Handle the "elvis" version of the ternary
-      // e.g., first ?: second  which is short for:  first != null ? first : second
-      condition = buildNotEquals( ExpressionTransformer.compile( _expr().getFirst(), _cc() ), nullLiteral() );
+      // e.g., <first> ?: <second>  which is short for:
+      // var temp = <first>
+      // temp != null ? temp : <second>
+      IRSymbol firstSym = _cc().makeAndIndexTempSymbol( getDescriptor( _expr().getFirst().getType() ) );
+      IRAssignmentStatement firstAssn = buildAssignment( firstSym, ExpressionTransformer.compile( _expr().getFirst(), _cc() ) );
+      return buildTernary(
+        buildComposite( firstAssn, buildNotEquals( identifier( firstSym ), nullLiteral() ) ),
+        identifier( firstSym ),
+        ExpressionTransformer.compile( _expr().getSecond(), _cc() ),
+        getDescriptor( _expr().getType() ) );
     }
     else
     {
-      condition = ExpressionTransformer.compile( _expr().getCondition(), _cc() );
+      return buildTernary(
+        ExpressionTransformer.compile( _expr().getCondition(), _cc() ),
+        ExpressionTransformer.compile( _expr().getFirst(), _cc() ),
+        ExpressionTransformer.compile( _expr().getSecond(), _cc() ),
+        getDescriptor( _expr().getType() ) );
     }
-    return buildTernary(
-      condition,
-      ExpressionTransformer.compile( _expr().getFirst(), _cc() ),
-      ExpressionTransformer.compile( _expr().getSecond(), _cc() ),
-      getDescriptor( _expr().getType() )
-    );
   }
 }
