4da9a63e8748fd5d1cca5ce41b3d4ca747cb4daa
Kyle Moore <github@kylemoore.com>
2015-08-12 15:55:58 -0700

    Default implementation of getIntrinsicType  * This change is required to execute the example in [example-gosuc-inverted](https://github.com/gosu-lang/example-gosuc-inverted)

--- gosu-core-api/src/main/java/gw/lang/reflect/gs/IGosuObject.java
+++ gosu-core-api/src/main/java/gw/lang/reflect/gs/IGosuObject.java
@@ -7,12 +7,15 @@ package gw.lang.reflect.gs;
 import gw.lang.Deprecated;
 import gw.lang.PublishInGosu;
 import gw.lang.reflect.IType;
+import gw.lang.reflect.TypeSystem;
 
 @PublishInGosu
 public interface IGosuObject
 {
   @Deprecated(value="Use the 'typeof' operator in Gosu instead. 'obj.IntrinsicType' becomes 'typeof obj'.")
-  IType getIntrinsicType();
+  default IType getIntrinsicType() {
+    return TypeSystem.get( getClass() );
+  }
 
   //
   // Methods cooresponding with java.lang.Object
