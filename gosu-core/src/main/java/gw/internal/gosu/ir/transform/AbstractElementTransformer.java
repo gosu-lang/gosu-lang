a93b3b233ae5150dc305fa72a83338f5df687681
smckinney <rsmckinney@hotmail.com>
2017-05-31 17:29:26 -0700

    GOSU-32: add fix dimension/big comparison compilation, add missing checkcast

--- gosu-core/src/main/java/gw/internal/gosu/ir/transform/AbstractElementTransformer.java
+++ gosu-core/src/main/java/gw/internal/gosu/ir/transform/AbstractElementTransformer.java
@@ -1805,7 +1805,7 @@ public abstract class AbstractElementTransformer<T extends IParsedElement>
   protected IRAssignmentStatement convertOperandToBig( IType bigType, Class bigClass, IType operandType, IRExpression operand, IRSymbol tempRet ) {
     IRAssignmentStatement tempOperandAssn;
     if( operandType == bigType ) {
-      tempOperandAssn = buildAssignment( tempRet, operand );
+      tempOperandAssn = buildAssignment( tempRet, checkCast( bigType, operand ) );
     }
     else if( operandType == JavaTypes.RATIONAL() )
     {
