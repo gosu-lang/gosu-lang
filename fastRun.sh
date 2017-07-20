bfb301923a929ecfdcd879f71ec00d6eb1619200
sadheesh <svenkatachalam@guidewire.com>
2017-07-20 13:05:18 -0700

    POC on test parallel for Gosu lang -1

---
+++ fastRun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+let nt=${CIRCLE_NODE_TOTAL}
+let ni=${CIRCLE_NODE_INDEX}
+
+
+if [ $ni -eq 0 ] ; then
+    echo "Running on first node "
+   mvn surefire:test -Dtest=*.*Test  -pl gosu-ant-tools -pl gosu-doc -pl gosu-lab -pl gosu-maven-compiler -pl gosu-core-api -B
+
+else
+    echo "Running the below test of rest of the nodes"
+    mvn surefire:test -Dtest=*.*Test -pl gosu-test
+
+fi
+
