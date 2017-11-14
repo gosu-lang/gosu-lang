36c1f236fb9c45cca6bf6c50a271a15c79e1e9af
smckinney <rsmckinney@hotmail.com>
2017-11-13 16:24:17 -0800

    fix dynamic loading of inner java class from java source

--- gosu-test/src/test/java/gw/lang/GosuScratchSuite.java
+++ gosu-test/src/test/java/gw/lang/GosuScratchSuite.java
@@ -24,7 +24,7 @@ public class GosuScratchSuite extends Suite
     BytecodeOptions.enableAggressiveVerification();
     return new GosuScratchSuite()
             .withTestEnvironment( new ScratchTestEnvironment() )
-      .withTest( "gw.lang.spec_old.generics.GenericMethodsTest" );
+      .withTest( "gw.specContrib.classes.inner.InnerClassTest" );
     //  .withTest( "gw.internal.gosu.parser.classTests.gwtest.dynamic.JsonTest" );
   }
 
