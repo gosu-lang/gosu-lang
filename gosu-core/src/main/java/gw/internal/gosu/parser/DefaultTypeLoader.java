226a88452b339f345fceb9d92080c354ae426a78
Kyle Moore <github@kylemoore.com>
2018-05-11 15:19:38 -0700

    Maybe fix situation where .class files were favored over sources in the current module

--- gosu-core/src/main/java/gw/internal/gosu/parser/DefaultTypeLoader.java
+++ gosu-core/src/main/java/gw/internal/gosu/parser/DefaultTypeLoader.java
@@ -207,7 +207,7 @@ public class DefaultTypeLoader extends SimpleTypeLoader implements IExtendedType
     // references a Gosu class e.g., in a method return type.  Gosu needs to get
     // the type info at the *IJavaClassInfo* level for the the Gosu class.
 
-    if( !ExecutionMode.isIDE() && classFileExists( fqn ) && !isExtendedClass( fqn ) )
+    if( ExecutionMode.isRuntime() && classFileExists( fqn ) && !isExtendedClass( fqn ) )
     {
       // If not in an IDE, favor .class files as the basis for Java types.
       //
