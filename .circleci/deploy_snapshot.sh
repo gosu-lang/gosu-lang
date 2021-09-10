a654db2d9278e8f6d6bcd395047172cfff596246
sadheesh <svenkatachalam@guidewire.com>
2021-09-09 19:00:50 -0700

    Test deploy11

--- .circleci/deploy_snapshot.sh
+++ .circleci/deploy_snapshot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-echo "Running Deploy job"
+
 if [ -z ${CI_PULL_REQUEST} ]; then # returns true if CI_PULL_REQUEST value is empty
+    echo "Running Deploy job"
    # mvn deploy -Dmaven.main.skip=true -Dmaven.resources.skip=true -Dmaven.test.skip=true -s settings.xml -B
 fi
