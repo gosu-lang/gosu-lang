bf99499cd41427b69c317db068a12a37321f71d9
sadheesh <svenkatachalam@guidewire.com>
2021-09-20 15:48:12 -0700

    circleci test 34

--- .circleci/update_version.sh
+++ .circleci/update_version.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 echo "Updating version job"
 echo "snaphsot version passed: $1"
+if [[ $1 == *-SNAPSHOT ]] ; then
+    echo "snaphsot version passed: $1"
+    snapshotversion=$1
+    mvn -B release:update-versions -DdevelopmentVersion=${snapshotversion}
+    else
+       echo " Version number should end with -SNAPSHOT"
+       exit 1
+fi
+git add -u
+git commit -m "Change version $1 for testing"
+git push
