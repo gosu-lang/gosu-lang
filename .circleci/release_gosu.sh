f7d8552fad2cc1601732207615c5c6d7f4e2c5d2
sadheesh <svenkatachalam@guidewire.com>
2021-09-20 11:18:39 -0700

    circleci test 32

--- .circleci/release_gosu.sh
+++ .circleci/release_gosu.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 echo "Release Gosu job"
+if [ -z ${CI_PULL_REQUEST} ]; then # returns true if CI_PULL_REQUEST value is empty
+    echo "Running Deploy job"
+   # mvn --batch-mode clean release:prepare release:perform
+   # mvn release:perform -B -s settings.xml
+fi
