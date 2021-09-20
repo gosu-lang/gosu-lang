d0dd58b4385be6f3faccf0cb9edf5973d8cc281b
sadheesh <svenkatachalam@guidewire.com>
2021-09-20 16:51:57 -0700

    circleci test 36

--- .circleci/update_version.sh
+++ .circleci/update_version.sh
@@ -4,11 +4,13 @@ echo "snaphsot version passed: $1"
 if [[ $1 == *-SNAPSHOT ]] ; then
     echo "snaphsot version passed: $1"
     snapshotversion=$1
+    echo ${snapshotversion}
     mvn -B release:update-versions -DdevelopmentVersion=${snapshotversion}
     else
        echo " Version number should end with -SNAPSHOT"
        exit 1
 fi
+echo "git operation started"
 git add -u
 git commit -m "Change version $1 for testing"
 git push
