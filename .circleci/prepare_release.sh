917e28543b4cd215297575431baaf1042762fee4
sadheesh <svenkatachalam@guidewire.com>
2021-09-21 19:39:23 -0700

    circleci test 46

--- .circleci/prepare_release.sh
+++ .circleci/prepare_release.sh
@@ -5,16 +5,15 @@ echo "git operation started"
 mkdir ~/.ssh/ && echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 git config user.email "reach.sadheesh@gmail.com"
 git config user.name "circleCi-bot"
-echo ">>>>>>>>>>>>>>>"
 gpg --list-keys
-echo "<<<<<<<<<<<<<<<<"
 
 if [[ $1 != *"SNAPSHOT"* ]] ; then
     releaseVersion=$1
     mvn release:prepare -B -DreleaseVersion=${releaseVersion}
+    mvn release:perform -B -s settings.xml
     else
        echo " Version number should not contain SNAPSHOT"
        exit 1
 fi
 # Running final release target
-#mvn release:perform -B -s settings.xml
+
